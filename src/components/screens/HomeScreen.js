d231ed532e855e9859f98c6bf4b4da6c71b25f77
Chris Aronchick <chrisaronchick@gmail.com>
2019-03-04 19:55:32 -0800

    Added modal

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -28,6 +28,8 @@ import {
   import apis from '../../apis/apis'
 
   import PresenteeScreen from './PresenteeScreen'
+  import PresenteeModal from './PresenteeModal'
+  import signInStyles from '../styles/signIn'
 
 export default class HomeScreen extends React.Component {
   state = {
@@ -35,6 +37,7 @@ export default class HomeScreen extends React.Component {
     userToken: '',
     presentorInfo: {},
     gettingPresentorInfo: true,
+    modalVisible: false
   }
 
   getPresentorInfo = async () => {
@@ -60,47 +63,83 @@ export default class HomeScreen extends React.Component {
   }
 
 
+  updatePresentee = async () => {
+    await Auth.currentAuthenticatedUser()
+    .then(async (user) => {
+        try {
+            console.log('this.state.presentee :', this.state.presentee);
+            await apis.updatePresenteeInfo(user.signInUserSession.idToken.jwtToken,this.state.presentee)
+            .then((presenteeInfo) => {
+                this.hideModal();
+                this.setState({presentee: presenteeInfo})
+            })
+        } catch (error) {
+            console.log('updatePresentee Error :', error);
+        }
+    })
+    .catch(userError => console.log('userError :', userError))
+  }
+  showModal() {
+      this.setState({ modalVisible: true })
+      console.log('Modal Shown')
+      console.log('modal state :', this.state)
+  }
+  hideModal() {
+      this.setState({ modalVisible: false })
+      // Refocus on phone Input after modal is closed
+      console.log('Modal Hidden')
+  }
+
+
 
   render() {
     let { presentorInfo } = this.state;
     return (
-      <View style={styles.container}>
-      {/* refresh button  */}
-        {!this.state.gettingPresentorInfo ? (
-          <Item rounded style={styles.itemStyle}>
-            <TouchableOpacity onPress={() => this.getPresentorInfo()}>
-              <Icon
-              active
-              name='refresh'
-              style={styles.iconStyle}
-              />
-            </TouchableOpacity>
-          </Item>
-        ) : null}
-        {presentorInfo && presentorInfo.presentees ? 
-          <FlatList
-            data={presentorInfo.presenteesArray}
-            keyExtractor={(item) => item.id}
-            renderItem={({item}) => (
-              <TouchableOpacity onPress={() => this.selectPresentee({ presentee: item})}>
-                <View key={item.id} style={[styles.container, styles.listItem]}>
-                  <Text>{item.firstName} {item.lastName}</Text>
-                </View>
+      <SafeAreaView style={signInStyles.container}>
+        <View style={styles.container}>
+        {/* refresh button  */}
+          {!this.state.gettingPresentorInfo ? (
+            <Item rounded style={styles.itemStyle}>
+              <TouchableOpacity onPress={() => this.getPresentorInfo()}>
+                <Icon
+                active
+                name='refresh'
+                style={styles.iconStyle}
+                />
               </TouchableOpacity>
-            )}
+            </Item>
+          ) : null}
+          {presentorInfo && presentorInfo.presentees ? (
+              <View style={styles.container}>
+                <FlatList
+                  data={presentorInfo.presenteesArray}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({item}) => (
+                    <Item style={styles.inputStyle}>
+                      <TouchableOpacity onPress={() => this.selectPresentee({ presentee: item})}>
+                        <View key={item.id} style={[styles.container, styles.listItem]}>
+                          <Text>{item.firstName} {item.lastName}</Text>
+                        </View>
+                      </TouchableOpacity>
+                    </Item>
+                  )}
+                  />
+                  <TouchableOpacity style={signInStyles.buttonStyle} onPress={() => this.showModal()}>
+                    <Text style={signInStyles.buttonText}>Add Giftees</Text>
+                  </TouchableOpacity>
+                </View>
 
-          />
-          : this.state.gettingPresentorInfo ? 
-            <View style={styles.container}>
-              <ActivityIndicator size="large" color="#fff" />
-            </View>
-            :
-            <View style={styles.container}>
-              <Text>Add Giftees</Text>
-            </View>
-        }
-        
-      </View>
+            )
+            : this.state.gettingPresentorInfo ? 
+              <View style={styles.container}>
+                <ActivityIndicator size="large" color="#fff" />
+              </View>
+              : null
+          }
+          
+          <PresenteeModal modalVisible={this.state.modalVisible} presentee={{}} onSave={() => this.updatePresentee()} onClose={() => this.hideModal()} />
+        </View>
+      </SafeAreaView>
     )
   }
 }
